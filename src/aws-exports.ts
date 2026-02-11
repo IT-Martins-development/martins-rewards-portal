413da2633013396c790d15dc222cdc309a154ece
Operations_Supplies <operations@Accountings-MacBook-Air.local>
2026-02-11 15:55:19 -0500

    fix: add aws-exports config (appsync + cognito)

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,8 +1,14 @@
-const awsconfig = {
+// src/aws-exports.ts
+const awsExports = {
   aws_project_region: "us-east-2",
+  aws_appsync_graphqlEndpoint: "https://wnpqbka2h5d3lfi3xznvmd4tcq.appsync-api.us-east-2.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-2",
+  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+
+  // Se você usa Cognito (quase certo), coloque também:
   aws_cognito_region: "us-east-2",
   aws_user_pools_id: "us-east-2_J1tQgVm42",
-  aws_user_pools_web_client_id: "43rdma1om3vhmovk5hdr7desr2"
+  aws_user_pools_web_client_id: "43rdma1om3vhmovk5hdr7desr2",
 };
 
-export default awsconfig;
+export default awsExports;
