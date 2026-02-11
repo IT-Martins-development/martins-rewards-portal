8843a97a628023b088ca0a3b5efc6d54c5bb30d5
Operations_Supplies <operations@Accountings-MacBook-Air.local>
2026-02-11 15:13:37 -0500

    chore: add aws-exports for frontend-only deploy

---
+++ src/aws-exports.ts
@@ -0,0 +1,8 @@
+const awsconfig = {
+  aws_project_region: "us-east-2",
+  aws_cognito_region: "us-east-2",
+  aws_user_pools_id: "us-east-2_J1tQgVm42",
+  aws_user_pools_web_client_id: "43rdma1om3vhmovk5hdr7desr2"
+};
+
+export default awsconfig;
