199aa5a6276e704e8beeb438c18f4d4c9617c915
Operations_Supplies <operations@Accountings-MacBook-Air.local>
2026-02-12 07:30:20 -0500

    feat: restrict user portal to Investor group and admin to AdminReward33s

---
+++ src/amplifyInit.ts
@@ -0,0 +1,5 @@
+// src/amplifyInit.ts
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports"; // ou amplifyconfiguration.json
+import "./amplifyInit"; // <-- MUITO IMPORTANTE vir antes do App
 import App from "./App";
 import "./index.css";
-
-Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
