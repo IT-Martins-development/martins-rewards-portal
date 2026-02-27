c16c847766dfe2b6cfe13b8af9717007baf81483
Operations_Supplies <operations@Accountings-MacBook-Air.local>
2026-02-27 15:32:38 -0500

    force fix: injetando chaves de autenticação cognito

---
+++ src/aws-exports.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+// Manual fix para restaurar o login - Versão Forçada
+const awsmobile = {
+    "aws_project_region": "us-east-2",
+    "aws_cognito_region": "us-east-2",
+    "aws_user_pools_id": "us-east-2_J1tQgVm42",
+    "aws_user_pools_web_client_id": "43rdma1om3vhmovk5hdr7desr2",
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://wnpqbka2h5d3lfi3xznvmd4tcq.appsync-api.us-east-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-2",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "API": {
+        "GraphQL": {
+            "endpoint": "https://wnpqbka2h5d3lfi3xznvmd4tcq.appsync-api.us-east-2.amazonaws.com/graphql",
+            "region": "us-east-2",
+            "defaultAuthMode": "userPool"
+        }
+    }
+};
+
+export default awsmobile;
