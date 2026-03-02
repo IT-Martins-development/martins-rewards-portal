88cfee8f4b9ed874283d0c0dbe200d0872a28006
Operations_Supplies <operations@Accountings-MacBook-Air.local>
2026-03-02 09:49:14 -0500

    atualziaco login

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,12 @@ Amplify.configure({
       endpoint: 'https://wnpqbka2h5d3lfi3xznvmd4tcq.appsync-api.us-east-2.amazonaws.com/graphql',
       region: 'us-east-2',
       defaultAuthMode: 'userPool'
+    },
+    REST: {
+      "operatorApi": {
+        endpoint: "https://d3g2ypezejhh8u.execute-api.us-east-2.amazonaws.com/staging", 
+        region: "us-east-2"
+      }
     }
   }
 });
