b99906e44275c071d494a0f7743cd61a45354afb
Operations_Supplies <operations@Accountings-MacBook-Air.local>
2026-03-02 10:20:34 -0500

    atualziaco login

--- src/App.tsx
+++ src/App.tsx
@@ -24,18 +24,14 @@ Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: 'us-east-2_J1tQgVm42',
-      userPoolClientId: '43rdma1om3vhmovk5hdr7desr2'
+      userPoolClientId: '43rdma1om3vhmovk5hdr7desr2',
+
     }
   },
   API: {
-    GraphQL: {
-      endpoint: 'https://wnpqbka2h5d3lfi3xznvmd4tcq.appsync-api.us-east-2.amazonaws.com/graphql',
-      region: 'us-east-2',
-      defaultAuthMode: 'userPool'
-    },
     REST: {
-      "operatorApi": {
-        endpoint: "https://d3g2ypezejhh8u.execute-api.us-east-2.amazonaws.com/staging", 
+      operatorApi: {
+        endpoint: "https://d3g2ypezejhh8u.execute-api.us-east-2.amazonaws.com/staging",
         region: "us-east-2"
       }
     }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import "./amplifyInit"; // <-- tem que vir antes de qualquer uso do Amplify
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
