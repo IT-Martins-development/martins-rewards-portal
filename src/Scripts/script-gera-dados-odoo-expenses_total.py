801b6e5d2afb8b218b61c3d021721df4f360dcfe
Claus Fromming <81712536+cfromming@users.noreply.github.com>
2026-04-07 16:21:13 -0300

    teste conectividade

--- src/Scripts/script-gera-dados-odoo-expenses_total.py
+++ src/Scripts/script-gera-dados-odoo-expenses_total.py
@@ -9,7 +9,7 @@ INGEST_URL = "https://2kg0lpfvda.execute-api.us-east-2.amazonaws.com/main/odoo/i
 # CONFIG
 # =========================================================
 
-VENDOR_BATCH_SIZE = 500
+VENDOR_BATCH_SIZE = 550
 STOCK_BATCH_SIZE = 500
 
 API_SLEEP_BETWEEN_REQUESTS = 0.03
